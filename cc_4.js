9345cff970fa6bb9154dd84517f01e30523405e2
Colin Forbes <forbesm@usf.edu>
2025-09-16 15:13:50 -0400

    Step 2, five products listed

--- cc_4.js
+++ cc_4.js
@@ -1,36 +1,42 @@
-// Coding Challenge 3b
+// Coding Challenge 4
 
-let inventory = [
-    {sku: "SKU-001",
-     name: "Book",
-     price: 18.50,
-     stock: 2
+let stock = [
+    {category: "Apparel",
+     name: "Shirt",
+     price: 25.00,
+     inventory: 18
      },
 
-    {sku: "SKU-002",
-      name: "Paperclip",
-      price: 2.30,
-      stock: 40
+    {category: "Groceries",
+     name: "Cauliflower",
+     price: 5.50,
+     inventory: 12
     },
 
-    {sku: "SKU-003",
-     name: "Shaving cream",
-     price: 12.50,
-     stock: 12
+    {category: "Electronics",
+     name: "Calculator Watch",
+     price: 50.99,
+     inventory: 30
     },
 
-    {sku: "SKU-004",
-     name: "Pool",
-     price: 2000.30,
-     stock: 8
+    {category: "Household",
+     name: "Chair",
+     price: 35.50,
+     inventory: 40
+    },
+
+    {category: "Apparel",
+     name: "Socks",
+     price: 8.00,
+     inventory: 8
     }
   ];
 
-//Log items
+//Discount loop by category
 console.log("Step 2:")
 inventory.forEach(obj => console.log(`${obj.sku} | ${obj.name} | \$${obj.price} | ${obj.stock}`))
 
-//Add new item
+//Discount by customer
 let newItemChipAndDip = {sku: "SKU-005",
      name: "Chip and dip",
      price: 30.00,
@@ -39,10 +45,10 @@ let newItemChipAndDip = {sku: "SKU-005",
 
 inventory.push(newItemChipAndDip);
 
-//Remove last item
+//Simulate purchases
 remove1 = inventory.pop();
 
-//Update price and stock
+//Log each product with discount
 inventory[1].price = 2.00;
 inventory[0].stock = 1;
 
@@ -50,7 +56,7 @@ console.log("\nStep 3: A sale on paperclips and -1 in book stock")
 inventory.forEach(obj => console.log(`${obj.sku} | ${obj.name} | \$${obj.price} | ${obj.stock}`))
 
 
-//ORDERS ARRAY
+//Log all product inventory
 
 let orders = [
   {orderId: "001",
